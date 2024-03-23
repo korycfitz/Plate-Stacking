556738b8c82907d8dc623d33e3c06daccc653b83
korycfitz <korycfitz@gmail.com>
2024-03-23 03:32:02 -0400

    initial commit

---
+++ PlateStacking.py
@@ -0,0 +1,14 @@
+# Answer these questions:
+
+# What are the steps?
+# What is the step sequence?
+# Which decisions need to be made?
+# What repeats (loops)?
+# What data is needed?
+# Which data types are appropriate?
+# Does creating a function simplify the problem?
+
+# 1. Stub up a function that will be used to add a plate to the top of the stack
+# 2. Stub up a function that will be used to print the plates to the terminal
+# 3. Stub up a function that will be used to remove 1-to-n plates from the top of the stack
+# 4. 
